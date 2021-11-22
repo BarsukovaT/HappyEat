83baa08be1c0db6912ed63009bebb4a5792023cd
Ry0zu <ryozu38@gmail.com>
2021-11-22 20:37:57 +0100

    css y script

--- JS/script.js
+++ JS/script.js
@@ -19,27 +19,39 @@ function posicionarMenu() {
     }
 }
 
-// LOGIN //
 
+// REGISTER Y LOGIN //
+
+function register() {
+    document.querySelector(".formulario_register").style.display = "block";
+    document.querySelector(".contenedor_login-register").style.left = "410px";
+    document.querySelector(".forumlario_login").style.display = "none";
+    document.querySelector(".caja_trasera_register").style.opacity = "0";
+    document.querySelector(".caja_trasera_login").style.opacity = "1";
+
+}
 
 function login() {
+    document.querySelector(".formulario_register").style.display = "none";
+    document.querySelector(".contenedor_login-register").style.left = "10px";
+    document.querySelector(".forumlario_login").style.display = "block";
+    document.querySelector(".caja_trasera_register").style.opacity = "1";
+    document.querySelector(".caja_trasera_login").style.opacity = "0";
 
-    var n = document.getElementById("user");
-    var p = document.getElementById("pass");
-    var e1 = document.getElementById("recipe");
-    var e2 = document.getElementById("plan");
-    var e3 = document.getElementById("list");
+}
 
-    if (n.value == "admin" && p.value == "admin") {
+// VALIDAR FORMULARIO REGISTRO
 
-        location.href = "file:///C:/xampp/htdocs/HappyEat/index.html";
-        document.getElementById("access").style.display = "none";
-        document.getElementById("perfil").style.display = "block";
-        e1.classList.remove("disabled");
-        e2.classList.remove("disabled");
-        e3.classList.remove("disabled");
+function validarFormulario() {
+    var name = document.getElementById('n_completo').value;
+    var correo = document.getElementById('correo').value;
+    var user = document.getElementById('user').value;
+    var con = document.getElementById('con').value;
 
-    } else {
-        document.getElementById("rojo").style.visibility = "visible";
+    if (name.length == 0 || correo.length == 0 || user.length == 0 || con.length == 0) {
+        alert('Por favor, rellene todos los campos del registro.');
+        return;
+
     }
+
 }
